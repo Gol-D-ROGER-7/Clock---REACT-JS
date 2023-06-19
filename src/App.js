831c2eea28e31df2e7d5e14c3754a3033cc6cccc
Gol-D-ROGER-7 <shelbyarthur874@gmail.com>
2023-06-19 07:57:04 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let time = new Date().toLocaleTimeString();
+
+  const [currentTime, setCurrentTime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time)
+  }
+
+  setInterval(updateTime, 1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hii">
+     <h1 className='main' >{currentTime}</h1>
+     <h3>(◍•ᴗ•◍)</h3>
     </div>
   );
 }
